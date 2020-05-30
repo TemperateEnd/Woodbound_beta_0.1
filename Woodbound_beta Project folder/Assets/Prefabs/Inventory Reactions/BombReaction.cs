14d8773813a1102b756303cb4fb720ceefaae88b
Reece Tarrant <rtarrant70998@gmail.com>
2020-05-30 16:04:34 +0100

    Assets: Prefabs
    Adds prefab folder

---
+++ Woodbound_beta Project folder/Assets/Prefabs/Inventory Reactions/BombReaction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombReaction : MonoBehaviour
+{
+    [SerializeField] GameObject bomb;
+    public void Use()
+    {
+        PlayerMovement Player = FindObjectOfType<PlayerMovement>();
+        Instantiate(bomb, Player.transform.Find("BombPlace").position, Quaternion.identity);
+    }
+    
+}
---
+++ Woodbound_beta Project folder/Assets/Prefabs/Log/logsPhysics.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class logsPhysics : MonoBehaviour
+{
+
+    public Sprite wetLog;
+    public Sprite Log;
+    public Sprite threequarter;
+    public Sprite half;
+    public Sprite onequarter;
+    public Sprite inWater;
+    public double health;
+    public double healthFull;
+    public bool isWet=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = healthFull;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This is when the log is hit and junk
+         if(health / healthFull >= 75&& isWet==false)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Log;
+        }
+        else if (health / healthFull >= 50 && health/healthFull <= 75 &&isWet==false )
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = threequarter;
+        }
+        else if (health / healthFull >= 25 && health / healthFull <= 50 && isWet==false)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = half;
+        }
+        else if (health / healthFull >= 0 && health / healthFull <= 25 && isWet==false)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = onequarter;
+        }
+         else if (isWet == true)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = inWater;
+        }
+          
+    }
+
+    public void Damage()
+    {
+        if (health > 0) {
+            health -= 25;
+        }
+        
+    }
+    public void Water()
+    {
+        isWet = !isWet;
+    }
+
+    
+}
